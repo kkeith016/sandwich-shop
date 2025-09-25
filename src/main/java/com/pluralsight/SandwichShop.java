403ce82e465ea5d5a1ef43a0b31ab939c0eb98cd
Kylee <kkeith.biz@gmail.com>
2025-09-25 09:53:05 -0400

    defining sandwich sizes.

---
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class SandwichShop {
+    public static void main(String[] args) {
+        //added scanner
+        Scanner scan = new Scanner(System.in);
+
+        // Sandwiches
+        double regular = 5.45;
+        double large = 8.95;
+
+        //defining sandwich sizes.
+        char regularprice = 'R';
+        char largeprice = 'L';
+        char size = scan.next().charAt(0);
+
+
+
+
+
+
+    }
+}
